9ab6051df4ebbc3e731c4425de11e615eafc68e8
screenkrik <112891700+Beberik@users.noreply.github.com>
2023-11-23 19:22:06 +0300

    mvmvmhhhh

---
+++ Assets/endlessFood.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class endlessFood : MonoBehaviour
+{
+    int cnt = 0;
+    public GameObject foodPref;
+    public Transform foodAnchor;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "ForFoodBox")
+        {
+            print("!");
+            if (cnt == 0)
+                Instantiate(foodPref, foodAnchor.position, foodAnchor.rotation);
+        }
+        else if (other.tag == "Food")
+            cnt++;
+
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Food")
+            cnt--;
+    }
+}
