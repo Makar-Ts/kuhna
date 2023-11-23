c1fd0dfea8b6172132dd278a7ca20c9a72e0677e
Makar Ts <tsivikov.makar@gmail.com>
2023-11-23 21:55:50 +0300

    +Добавил кухню и запек свет

--- Assets/endlessFood.cs
+++ Assets/endlessFood.cs
@@ -4,23 +4,21 @@ using UnityEngine;
 
 public class endlessFood : MonoBehaviour
 {
-    int cnt = 0;
+    int foodCount = 0;
     public GameObject foodPref;
     public Transform foodAnchor;
-    private void OnTriggerEnter(Collider other)
-    {
-        if(other.tag == "ForFoodBox")
-        {
-            if (cnt == 0)
-                Instantiate(foodPref, foodAnchor.position, foodAnchor.rotation);
-        }
-        else if (other.tag == "Food")
-            cnt++;
 
+    private void OnTriggerEnter(Collider other) {
+        if(other.tag == "Food") {
+            foodCount++;
+        }
     }
-    private void OnTriggerExit(Collider other)
-    {
-        if(other.tag == "Food")
-            cnt--;
+
+    private void OnTriggerExit(Collider other) {
+        if (other.tag == "Food") {
+            foodCount--;
+
+            if (foodCount == 0) Instantiate(foodPref, foodAnchor.position, foodAnchor.rotation);
+        }
     }
 }
